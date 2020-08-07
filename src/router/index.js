f9a6c762517ad43efae9a29b05d3f82adaf4b8e8
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-06 21:25:21 -0500

    adding the signup page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test.vue'
+import Signup from '../views/Signup.vue';
 
 Vue.use(VueRouter)
 
@@ -24,8 +25,13 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup
+  }
 
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
