8ffcba4455a7c4a09ad23eefc52558c74bbba0ee
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-10 19:38:28 -0500

    adding the destroy action

--- src/router/index.js
+++ src/router/index.js
@@ -54,7 +54,7 @@ const routes = [
     path: "/posts/:id/edit",
     name: "PostsEdit",
     component: PostsEdit
-  }
+  },
 
 
 ];
--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <h1>title: {{  post.title  }}</h1>
+    <p><button v-on:click="deletePost()">Delete the post</button></p>
   </div>
 </template>
 
@@ -26,6 +27,13 @@ export default {
       console.log(this.$route.params.id);
 
       axios.get;
+    },
+    deletePost: function () {
+      console.log("deleting the post......");
+      axios.delete(`/api/posts/${this.$route.params.id}`).then((response) => {
+        console.log(response.data);
+        this.$router.push("/posts");
+      });
     },
   },
 };
